b4272858e5378e12822c26d33788f9cb8982a1bf
ychinata <ychinata@qq.com>
2022-11-16 07:04:02 +0800

    tmp

--- 3_radar_demo/2_lfmcw_basic_naughty_0503/lfmcw_basic.m
+++ 3_radar_demo/2_lfmcw_basic_naughty_0503/lfmcw_basic.m
@@ -24,17 +24,18 @@ endle_time=6.3e-6;          %空闲时间
 f_IFmax= (slope*2*maxR)/c ; %最高中频频率
 f_IF=(slope*2*r0)/c ;       %当前中频频率
 
-Nd=128;                                     % chirp数量 
-Nr=1024;                                    % ADC采样点数
-vres=(c/fc)/(2*Nd*(Tchirp+endle_time));     % 速度分辨率
+Nd = 128;                                   % chirp数量 
+Nr = 1024;                                  % ADC采样点数
+vres = (c/fc)/(2*Nd*(Tchirp+endle_time));   % 速度分辨率
 % Nr=1024*256;                              % 和频信号点数设置
 Fs=Nr/Tchirp;                               % 模拟信号采样频率
 
 t = linspace(0,Nd*Tchirp,Nr*Nd);            % 发射信号和接收信号的采样时间，在MATLAB中的模拟信号是通过数字信号无限采样生成的。
+                                            % 产生Nr*Nd个点
 
-Tx=zeros(1,length(t));      % 发射信号
-Rx=zeros(1,length(t));      % 接收信号
-Mix = zeros(1,length(t));   % 差频、差拍、拍频、中频信号
+Tx = zeros(1,length(t));      % 发射信号
+Rx = zeros(1,length(t));      % 接收信号
+Mix = zeros(1,length(t));     % 差频、差拍、拍频、中频信号
 
 r_t = zeros(1,length(t));
 td = zeros(1,length(t));
@@ -48,48 +49,50 @@ for i=1:length(t)
     r_t(i) = r0 + v0*t(i); % 距离更新
     td(i) = 2*r_t(i)/c;    % 延迟时间
     
-    Tx(i) = cos(2*pi*(fc*t(i) + (slope*t(i)^2)/2)); % 发射信号 实数信号
-    Rx(i) = cos(2*pi*(fc*(t(i)-td(i)) + (slope*(t(i)-td(i))^2)/2)); %接收信号 实数信号
+    % 实数信号
+    Tx(i) = cos(2*pi*(fc*t(i) + (slope*t(i)^2)/2)); % 发射信号 
+    Rx(i) = cos(2*pi*(fc*(t(i)-td(i)) + (slope*(t(i)-td(i))^2)/2)); %接收信号, t(i)-td(i)
     
-    if i<=1024
-         freq(i) = fc + slope*i; %发射信号时频图 只取第一个chirp
-         freq_echo(i) = fc + slope*i;%回波信号频谱延迟
+    if i <= Nr      % 1024
+         freq(i) = fc + slope*i;        % 发射信号时频图 只取第一个chirp
+         freq_echo(i) = fc + slope*i;   % 回波信号频谱延迟      % 与freq相同?长度会不会设置过长
     end
 
     Mix(i) = Tx(i).*Rx(i);%差频、差拍、拍频、中频信号
 end
 
-%发射信号时域图
+% fig1.发射信号时域图
 figure;
 plot(Tx(1:1024));
 xlabel('点数');
 ylabel('幅度');
-title('TX发射信号时域图');
+title('TX发射信号时域图(第1个chirp)');
 
-%发射信号时频图
+% fig2.发射信号时频图
 figure;
 plot(t(1:1024),freq(1:1024));
 xlabel('时间');
 ylabel('频率');
-title('TX发射信号时频图');
+title('TX发射信号时频图(第1个chirp)');
 
-%接收信号时域图
+% fig3.接收信号时域图
 figure;
 plot(Rx(1:1024));
 xlabel('点数');
 ylabel('幅度');
-title('RX接收信号时域图');
+title('RX接收信号时域图(第1个chirp)');
 
-%接收信号与发射信号的时频图
+% fig4.接收信号与发射信号的时频图
 figure;
 plot(t(1:1024),freq(1:1024));
 hold on;
-plot(td(1:1024)+t(1:1024),freq(1:1024),'r');
+plot(t(1:1024)+td(1:1024),freq(1:1024),'r');
 xlabel('时间');
 ylabel('频率');
-title('接收信号与发射信号时频图');
+title('接收信号与发射信号时频图(第1个chirp)');
 legend ('TX','RX');
 
+% fig5.
 figure;
 plot(db(abs(fft(Mix(1:1024)))));%查看宽带的和频信号 将chirp的点数改为1024*256即可看到有一个门信号，但注意计算机内存。
 xlabel('频率');
