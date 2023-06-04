74d4410f5801a349ebe36b86407ca98dac8941e5
ychinata <ychinata@qq.com>
2023-06-04 23:25:52 +0800

    No.: 3_radar_demo_3_6b208 Author:xy Description:不抽采，发现看不到0.6m有能量高点。可以尝试继续处理。 也需要按照论文 [53]的配置重新采数。

---
+++ 3_radar_demo/3_radar_6b208/radar208_breath_debug_0603.m
@@ -0,0 +1,126 @@
+% 采用24GHz InnoSenT 平台,1T1R
+% 原始数据不抽样
+% 2023.6.3
+tic
+%% ========================================================================
+clc;
+clear;
+close;
+% 疑问：
+
+%% 读取数据部分
+numADCSamples = 250;    % number of ADC samples per chirp
+% numADCBits = 16;        % number of ADC bits per sample, 通过时域波形看到没有饱和，暂不处理
+numRX = 1;           % number of receivers
+% numLanes = 2;        % do not change. number of lanes is always 2，用不到
+isReal = false;      % set to true if real only data, false if complex data
+
+%% 雷达参数设置
+Fs = 5e5 / 2;           % ADC采样率 见配置说明，iq两路需要减半为250kHz
+c = 3*1e8;              % 光速
+ts = numADCSamples/Fs;  % ADC采样时间
+% slope = B_valid / ts; % 调频斜率2.5e11
+B_valid = 0.25e9;       % 有效带宽,24.00-24.25GHz,250MHz
+delta_R = c/(2*B_valid);% 距离分辨率,0.6m
+t_frame = 0.05;         % 慢时间轴采样20Hz
+
+%% 读取Bin文件 2023.4.28
+% txt数据格式：
+% Tims      AI00(I) AI01(Q)      
+% 0.00000	2610.17	2380.37
+% 0.00400	2622.07	2337.95
+% 0.00800	2626.34	2303.47
+% 0.01200	2628.48	2260.74
+% 0.01600	2630.92	2221.07
+%%
+% 20230428
+% filename = 'data/0403-1_ds-338750.txt';     % 抽样50倍的数据
+% adcDataRaw = importdata(filename);
+% x_ds = adcDataRaw(:, 1);
+% y_ids = adcDataRaw(:, 2);
+% y_qds = adcDataRaw(:, 3);
+
+% 20230603
+filename = 'data/0403-1.txt';     % 原始数据
+rawdata = importdata(filename);
+adcDataRaw_ori = rawdata.data;
+adcDataRaw = adcDataRaw_ori(1:1e6, :);                 % 对于不抽样数据，则先取前1M个数据-20230603
+
+%% 若设置的ADC位宽超过xbit，则进行截取处理：忽略这一步
+% 排列IQ数据
+fileSize = size(adcDataRaw, 1);
+PRTnum = fix(fileSize/(numADCSamples*numRX));       % fix向下取整，4096
+fileSize = PRTnum * numADCSamples * numRX;          % 取整之后3276800
+% adcData = adcDataRaw(1:3e5, :);  % 手动取整为300k
+adcData = adcDataRaw(1:fileSize, :);  % 自动取整
+
+% InnoSenT 原始数据格式与TI不同，需要重写
+if isReal
+    % todo
+else
+    numChirps = fileSize/numADCSamples/numRX;     % 为
+    LVDS = zeros(1, fileSize/1);            % 一行数据包括I和Q
+    %combine real and imaginary part into complex data
+    %read in file: 2I is followed by 2Q
+    counter = 1;
+    for i=1:1:fileSize
+        LVDS(1,counter) = adcData(i,2) + sqrt(-1)*adcData(i,3);     % rename为adcDataIq
+%         LVDS(1,counter) = adcData(i) + sqrt(-1)*adcData(i+2);
+%         LVDS(1,counter+1) = adcData(i+1)+sqrt(-1)*adcData(i+3); 
+        counter = counter + 1;
+    end
+    % create column for each chirp
+    LVDS = reshape(LVDS, numADCSamples*numRX, numChirps);
+    %each row is data from one chirp
+    LVDS = LVDS.';      % size:(numChirps, numADCSamples*numRX), 比如1200 个Chirps, 每个Chirps有250*1(Ch)个数据 
+end
+
+%% 重组数据
+adcData = zeros(numRX,numChirps*numADCSamples);     % size:(4*409600)
+for row = 1:numRX           % 填充4行
+    for i = 1: numChirps    % 填充2048列
+        % adcData每次填充200个数据：1-200, 201-400...
+        % LVDS每行的数据：1-200, 201-400, 401-600, 601-800
+        adcData(row, (i-1)*numADCSamples+1:i*numADCSamples) = LVDS(i, (row-1)*numADCSamples+1:row*numADCSamples);
+    end
+end
+% 200*2048
+retVal = reshape(adcData(1, :), numADCSamples, numChirps); %取第二个接收天线数据，数据存储方式为一个chirp一列
+% 200*1024, 1T4R只取一半列数据, InnoSenT不做此操作，照搬数据
+process_adc = zeros(numADCSamples, numChirps);
+gap = 1;
+for nchirp = 1:gap:numChirps  %1T4R 
+    process_adc(:, (nchirp-1)/gap+1) = retVal(:,nchirp);
+end
+
+%% 距离维FFT（1个chirp)
+% 为了减少图像显示,暂时注释掉
+% 若目标距离1.5m，则在1.5m处会出现一个峰值
+% %{
+figure;
+n = 50;     % 在N个chip中取第n个chirp并显示
+plot((1:numADCSamples)*delta_R, db(abs(fft(process_adc(:,n))))); 
+xlabel('距离（m）');
+ylabel('幅度(dB)');
+% title('Fig.1.距离维FFT(1个chirp)(抽样50倍)');
+title('Fig.1.距离维FFT(1个chirp)');
+
+figure;
+plot(db(abs(fft(process_adc(:,n)))))
+xlabel('样点数');
+ylabel('幅度(dB)');
+% title('Fig.2.距离维FFT(1个chirp)(抽样50倍)');
+title('Fig.2.距离维FFT(1个chirp)');
+
+figure;
+plot(real(process_adc(:,n)),'b');
+hold on;
+plot(imag(process_adc(:,n)),'r')
+xlabel('样点数');
+ylabel('幅度');
+% title('Fig.2-1.一个chirp时域(抽样50倍)');
+title('Fig.2-1.一个chirp时域');
+% %}
+
+%%
+toc
